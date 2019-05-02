43deb2063d81fa3e474fb0b8bd6b907606f0f8b0
Yue Tan <yuetan@cs.princeton.edu>
2019-05-02 12:05:35 -0400

    mv cc files to subdirectory and upate rust power law to take cmdline args

---
+++ cc/power-law.cc
@@ -0,0 +1,98 @@
+#include "rocksdb/db.h"
+#include <cassert>
+#include <iostream>
+#include <cstdlib>
+#include <climits>
+#include <string>
+#include "random.h"
+
+using namespace rocksdb;
+
+// constants
+#define rand_min 1
+#define rand_max (1024 << 10 - 1) // 2^20
+#define value_size (1024 * 4) // 4KB
+
+inline Slice* BigSlice(size_t size) {
+  char* buf = new char[size];
+  memset(buf, 1, size);
+  return new Slice(buf, size);
+}
+
+inline void IntToSlice (Slice& s, char* buf, uint64_t input) {
+  buf[7] =  input & 0xff;
+  buf[6] = (input >> 8) & 0xff;
+  buf[5] = (input >> 16) & 0xff;
+  buf[4] = (input >> 24) & 0xff;
+  buf[3] = (input >> 32) & 0xff;
+  buf[2] = (input >> 40) & 0xff;
+  buf[1] = (input >> 48) & 0xff;
+  buf[0] = (input >> 56) & 0xff;
+  s = Slice(buf, sizeof(input));
+}
+
+int main(int argc, char* argv[]){
+    //assert(argc == 2);
+    //std::string index(argv[1]);
+    std::string db_name = "/disk/scratch1/ty/testdb"/* + index*/;
+    //open a DB
+    DB * db;
+    Options options;
+    DestroyDB(db_name, options);
+    options.create_if_missing = true;
+    options.error_if_exists = true;
+    options.write_buffer_size = 4 << 20; // use 4MB memtable to force compaction
+    options.statistics = CreateDBStatistics();
+    Status status = DB::Open(options, db_name, &db);
+    assert(status.ok());
+
+    uint64_t key;
+    Slice key_slice;
+    char * buf = new char[8];
+    rocksdb::WriteOptions wo;
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        key = (uint64_t)std::stol(line);
+        IntToSlice(key_slice, buf, key);
+        status = db->Put(wo, key_slice, *BigSlice(value_size));
+    }
+
+    //report
+    std::string out;
+    std::cout << "Memtable Size: " << (options.write_buffer_size >> 20) << " MB" << std::endl;
+    db->GetProperty("rocksdb.num-immutable-mem-table", &out);
+    std::cout << "# immutable memtables not yet flushed: " << out << std::endl;
+    //db->GetProperty("rocksdb.num-immutable-mem-table-flushed", &out);
+    //std::cout << "# immutable memtables already flushed: " << out << std::endl;
+    db->GetProperty("rocksdb.mem-table-flush-pending", &out);
+    if (out == "0") {
+        std::cout << "No pending memtable flushing" << std::endl;
+    } else {
+        db->GetProperty("rocksdb.num-running-flushes", &out);
+        std::cout << "There are " + out + " pending memtable flushings" << std::endl;
+    }
+    
+    db->GetProperty("rocksdb.compaction-pending", &out);
+    if (out == "0") {
+        std::cout << "No pending compaction" << std::endl;
+    } else {
+        db->GetProperty("rocksdb.num-running-compactions", &out);
+        std::cout << "There are " + out + " running compactions" << std::endl;
+    }
+    // Levels
+    db->GetProperty("rocksdb.levelstats", &out);
+    std::cout << out << std::endl;
+    // Detailed statistics (see include/statistics.h)
+    db->GetProperty("rocksdb.options-statistics", &out);
+    std::cout << out << std::endl;
+    //std::cout << "Total Puts: " << putting_count << std::endl;
+    //std::cout << "Total failed Puts: " << putting_count_fail << std::endl;
+    //std::cout << "Total successful Puts: " << putting_count - putting_count_fail << std::endl;
+    //std::cout << "Total Puts in Bytes: " << (long)putting_count * value_size << std::endl;
+    
+    delete db;
+    DestroyDB("/disk/scratch1/ty/testdb", options);
+
+    return 0;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,52 @@
 extern crate zipf;
+#[macro_use(crate_version, crate_authors)]
+extern crate clap;
+
+use clap::{App, Arg};
+
 use zipf::ZipfDistribution;
-use std::env;
+
 use rand::thread_rng;
 use rand::distributions::Distribution;
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let alpha: f64 = args[1].parse().unwrap();
-	let xmax: usize = 2 << 20;
+	let cmd_args = App::new("power-law")
+		.version(crate_version!())
+		.author(crate_authors!())
+		.about("Generate a sample according to power law distribution")
+		.arg(
+			Arg::with_name("alpha")
+				.short("a")
+				.long("alpha")
+				.takes_value(true)
+				.required(true)
+				.help("Alpha parameter of the power law distribution")
+		)
+		.arg(
+			Arg::with_name("max-val")
+				.short("m")
+				.long("maxval")
+				.takes_value(true)
+				.required(true)
+				.help("Maximum value of the sample")
+		)
+		.arg(
+			Arg::with_name("num-points")
+				.short("n")
+				.long("numpoints")
+				.value_name("NUMPOINTS")
+				.takes_value(true)
+				.required(true)
+				.help("Total number of points in the sample")
+		)
+		.get_matches();
+		
+	let alpha = cmd_args.value_of("alpha").unwrap().parse::<f64>().unwrap();
+	let xmax = cmd_args.value_of("max-val").unwrap().parse::<usize>().unwrap();
+	let num = cmd_args.value_of("num-points").unwrap().parse::<usize>().unwrap();
+
 	let dist: ZipfDistribution = ZipfDistribution::new(xmax, alpha).unwrap();
-	for _ in 0..xmax {
+	for _ in 0..num {
 		println!("{}", dist.sample(&mut thread_rng()));
     	}
 }
